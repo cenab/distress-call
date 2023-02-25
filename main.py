0fb934b0a25a36f92accd35a1e74374fa8a8ac04
cenab <40250311+cenab@users.noreply.github.com>
2023-02-25 07:20:43 -0700

    final push

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 import cohere
 from cohere.classify import Example
 
+import geocoder
+
+import smtplib, ssl
+
+port = 465  # For SSL
+password = "helpme123"
 co = cohere.Client('ib4Rc6soXLKKinhSdbE7trjG1Gupc2ZuSLtUODa6')
+
+
+
 
 examples=[
   Example("Help me", "negative"), 
@@ -15,7 +23,8 @@ examples=[
   Example("I cannot move", "negative"), 
   Example("I cannot breath", "negative"), 
   Example("I need help", "negative"), 
-  Example("I am ok", "positive"), 
+  Example("I am ok", "positive"),
+  Example("There is somebody following me", "negative"),  
   Example("We have got food", "positive"), 
   Example("I am away from the buildings", "positive"), 
   Example("Get away from the buildings", "neutral"), 
@@ -23,29 +32,37 @@ examples=[
   Example("when is rescue team coming", "negative"),
 ]
 
+
+
 app = FastAPI()
 
 
 
-
-@app.get("/")
-def read_root():
+# Path: /{email_of_friend}/{status} example: www.distresscall.com/johndoe@gmail.com/i%20am%20stuck
+@app.get("/{email_of_friend}}/{status}")
+def read_root(request: Request, email_of_friend: str, status: str):
     voice_message = ""
     inputs=[voice_message]
     response = co.classify(
     inputs=inputs,
     examples=examples,
     )
+    
+
+    g = geocoder.ip(client_host = request.client.host)
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("distresscallapp12314124@gmail.com", password)
+        
     if response == "positive":
         return {"status": "safe"}
     elif response == "neutral":
+        message = "need help, please come to my location: " + g.latlng
+        server.sendmail("distresscallapp12314124@gmail.com", email_of_friend, message)
         return {"status": "need help"}
-    elif response == "negative":
+    else:
+        message = "need immidiate help, please come to my location: " + g.latlng
+        server.sendmail("distresscallapp12314124@gmail.com", email_of_friend, message)
         return {"status": "need immidiate help"}
     
-    return {"": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
